7bb8dfc91c0d305b9288d4b6cf9457fb7aa9797e
Tim <timothynguono@gmail.com>
2023-05-19 13:14:58 +0300

    Add: Book's words

--- app/src/main/java/com/example/kitaplamo/FirstFragment.kt
+++ app/src/main/java/com/example/kitaplamo/FirstFragment.kt
@@ -35,20 +35,112 @@ class FirstFragment : Fragment() {
 
 
         binding.okinyi.setOnClickListener {
-
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment , Bundle().apply{ putInt("lamoId",0)}
-            )
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment , Bundle().apply{ putInt("lamoId",0)})
         }
 
         binding.odhiambo.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply { putInt("lamoId",1) })
+        }
+
+        binding.odhiamboMoniang.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment,Bundle().apply {putInt("lamoId",2)})
         }
 
         binding.batiso.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{putInt("lamoId",3)})
         }
-        binding.sao.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+        binding.batisoMantieKiawa.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment,Bundle().apply { putInt("lamoId",4) } )
+        }
+
+        binding.batisoMaApoya.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",5)})
+        }
+
+        binding.katkaismManyien.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply { putInt("lamoId",6) })
+        }
+        binding.rwakoJiESawo.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply { putInt("lamoId",7) })
+        }
+        binding.yiewoLwedo.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",8)})
+        }
+        binding.pogoSawo.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",9)})
+        }
+        binding.kendMaler.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",10)})
+        }
+        binding.guedhoKend.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",11)})
+        }
+        binding.nuoyoKuongruokMarKend.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",12)})
+        }
+        binding.jumaKaJuma.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",13)})
+        }
+        binding.miseMarKanisa.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",14)})
+        }
+        binding.waloKanisa.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",15)})
+        }
+        binding.duokoKanisaETijeMamoko.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",16)})
+        }
+        binding.yiko.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",17)})
+        }
+        binding.yikMachielo.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",18)})
+        }
+        binding.yikoNgatModere.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",19)})
+        }
+        binding.rwakoJopuonjKanisa.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",20)})
+        }
+        binding.pwodhoGikLemoMogak.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",21)})
+        }
+        binding.rwakoJokristoMoaKaniseMamoko.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",22)})
+        }
+        binding.rwakoJokristoManeOa.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",23)})
+        }
+        binding.lokruokAERicho.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",24)})
+        }
+        binding.limoJotuo.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",25)})
+        }
+        binding.chang.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",26)})
+        }
+        binding.ndaloMoyiedhi.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",27)})
+        }
+        binding.mamoko.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",28)})
+        }
+        binding.erokamano.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",29)})
+        }
+        binding.erokamanoBangGimoroMotimore.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",30)})
+        }
+        binding.erokamanoBangKeyo.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",31)})
+        }
+        binding.erokamanoBangNyuol.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",32)})
+        }
+        binding.goyoErokamano.setOnClickListener{
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, Bundle().apply{ putInt("lamoId",33)})
         }
     }
 
